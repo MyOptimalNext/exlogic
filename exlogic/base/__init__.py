8a860bd376bcfce75168403b3e99340c0d528ac1
MyOptimalNext <loukmanehadjsaid56@gmail.com>
2025-07-28 11:11:13 +0100

    Update __init__.py

--- exlogic/base/__init__.py
+++ exlogic/base/__init__.py
@@ -1,5 +1,11 @@
-from .eOperations import eadd, emultiply, epower
+from .eOperations import eadd, esubtract, emultiply, edivide, ereciprocal
+
+# مؤقتاً حتى تعيد إضافة epower
+def epower(*args, **kwargs):
+    raise NotImplementedError("epower تم إزالتها مؤقتاً")
 
 __all__ = ['eadd',
+           'esubtract',
            'emultiply',
-           'epower']
+           'edivide', 
+           'ereciprocal',]
