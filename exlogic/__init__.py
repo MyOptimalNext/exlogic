a9fe1f025f17b888161d799d2efca079e0b63820
MyOptimalNext <loukmanehadjsaid56@gmail.com>
2025-07-28 11:12:53 +0100

    Update __init__.py

--- exlogic/__init__.py
+++ exlogic/__init__.py
@@ -1,10 +1,12 @@
 from .eEncoder import float32_to_comp64
 from .eDecoder import comp64_to_float32
-from .base import eadd, emultiply, epower
+from .base import eadd, esubtract, emultiply, edivide, ereciprocal
 
 __version__ = "0.1.0"
 __all__ = ['float32_to_comp64',
            'comp64_to_float32',
            'eadd',
+           'esubtract',
            'emultiply',
-           'epower']
+           'edivide',
+           'ereciprocal']
